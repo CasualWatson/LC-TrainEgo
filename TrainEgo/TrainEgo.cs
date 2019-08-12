e420726b81a4dd502d06ec9336f148db91d9cde2
CasualWatson <i3watson@tuta.io>
2019-08-11 23:12:02 -0500

    Current Code

---
+++ TrainEgo/TrainEgo.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+// using UnityEngine;
+
+namespace TrainEgo
+{
+    public class HellTrainWeapon : EquipmentScriptBase
+    {
+        private class HellTrainBuff : UnitBuf
+        {
+            private BarrierEffect barrier;
+            public HellTrainBuff()
+            {
+                duplicateType = BufDuplicateType.ONLY_ONE;
+                type = UnitBufType.BARRIER_B;
+                remainTime = 15f;
+            }
+
+            public override void Init(UnitModel model)
+            {
+                base.Init(model);
+                barrier = BarrierEffect.MakeBarrier(model as WorkerModel);
+                barrier.SetRemainRate(1f);
+                barrier.SetRwbpType(RwbpType.B);
+            }
+            public override float OnTakeDamage(UnitModel attacker, DamageInfo damageInfo)
+            {
+                if (damageInfo.type == RwbpType.B)
+                    return -1f;
+                return base.OnTakeDamage(attacker, damageInfo);
+            }
+            public override void OnDestroy()
+            {
+                base.OnDestroy();
+                if (remainTime <= 0f)
+                    barrier.OnDisappear();
+                else
+                    barrier.OnBreak();
+            }
+
+            public override void OnUnitDie()
+            {
+                base.OnUnitDie();
+                Destroy();
+            }
+            public override void OnUnitPanic()
+            {
+                base.OnUnitPanic();
+                Destroy();
+            }
+        }
+        public override bool OnGiveDamage(UnitModel actor, UnitModel target, ref DamageInfo dmg)
+        {
+            // Should make the weapon do no damage
+            dmg = DamageInfo.zero;
+            // Activate buff to absorb BLACK damage
+            if (!actor.HasUnitBuf(UnitBufType.BARRIER_B))
+                actor.AddUnitBuf(new HellTrainBuff());
+            return base.OnGiveDamage(actor, target, ref dmg);
+        }
+
+        /*
+        public override bool OnTakeDamage_After(float value, RwbpType type)
+        {
+            if (type == RwbpType.B)
+                value = -value;
+
+            return base.OnTakeDamage_After(value, type);
+        }
+        */
+    }
+    public class Harmony_Patch
+    {
+        public static int totalTickets = 0;
+        public static int ticketsRequired = 20;
+
+        public Harmony_Patch()
+        {
+            try
+            {
+                HarmonyInstance hInstance = HarmonyInstance.Create("Lobotomy.S-Purple & Watson & NEET.TrainEgo");
+                hInstance.PatchAll(Assembly.GetExecutingAssembly());
+                FileLog.Log("TrainEgo Patching Successful");
+            }
+            catch (Exception)
+            {
+                FileLog.Log("TrainEgo Patching Threw an Exception...");
+            }
+        }
+
+        [HarmonyPatch(typeof(HellTrain), "OnStageStart")]
+        private class HellTrain_OnStageStart_Patch
+        {
+            static void Prefix(HellTrain __instance)
+            {
+                __instance.Unit.model.metaInfo.qliphothMax = 999;
+            }
+            static void Postfix()
+            {
+                totalTickets = 0;
+            }
+        }
+
+        [HarmonyPatch(typeof(HellTrain), "SellTicket")]
+        private class HellTrain_SellTicket_Patch
+        {
+            static void Prefix(HellTrain __instance)
+            {
+                totalTickets += (int)AccessTools.Property(typeof(HellTrain), "_otherCreatureWorkCount").GetValue(__instance);
+                __instance.model.SetQliphothCounter(totalTickets);
+            }
+            static void Postfix()
+            {
+                if (totalTickets >= ticketsRequired)
+                    Present();
+            }
+        }
+
+        private static void Present()
+        {
+            int armorID = 0; // Armor ID
+            if (InventoryModel.Instance.CheckEquipmentCount(armorID))
+                InventoryModel.Instance.CreateEquipment(armorID);
+
+            int wepID = 0; // Weapon ID
+            if (InventoryModel.Instance.CheckEquipmentCount(wepID))
+                InventoryModel.Instance.CreateEquipment(wepID);
+        }
+
+        [HarmonyPatch(typeof(HellTrain), "HasRoomCounter")]
+        private class HellTrain_HasRoomCounter_Patch
+        {
+            static void Postfix(ref bool __result)
+            {
+                // Prevents any bugs involving lowering the Qliphoth counter
+                __result = false;
+            }
+        }
+    }
+}
